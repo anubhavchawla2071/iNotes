7707aaf4d5662e9e7ac02edb585ff0fd214d4f9d
Anubhav <anubhavchawla02@gmail.com>
2022-07-15 23:16:05 +0530

    updated port config

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ connectToMongo(process.env.MONGO);
 
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
